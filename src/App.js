f8c793b93a971a96f7c48b9a399b74f999e23051
Cognus <cognusdev@gmail.com>
2020-07-25 23:52:47 -0300

    update until at lesson css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 
+import './App.css'
+
+import backgroundImage from './assets/computers.jpeg'
+
 import Header from './components/Header'
 
 
@@ -25,6 +29,8 @@ function App() {
     return (
         <>
             <Header title="Projects" />
+
+            <img width={300} src={backgroundImage} />
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
             </ul>
